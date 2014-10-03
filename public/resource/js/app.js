e9129ebc6282cca9149d5160279311d75b635375
Jiahang Li <jiahang_li@outlook.com>
2014-10-03 08:08:48 -0700

    [Tak] Reset still not work; UI improved

--- public/resource/js/app.js
+++ public/resource/js/app.js
@@ -4,13 +4,16 @@
 
 var visualSSH = angular.module('visualSSH', []);
 
-visualSSH.controller('infoCtrl', ['$scope', '$http', function($scope, $http){
+visualSSH.controller('infoCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
     $scope.host = "";
     $scope.user = "";
     $scope.pass = "";
-    $scope.data = "";
+    $scope.data = [];
     $scope.file = "";
+    $scope.canGoBack = false;
+    $scope.connected = false;
 
+    var history = [];
     var _dir = "";
     $scope.executions = 0;
 
@@ -23,35 +26,75 @@ visualSSH.controller('infoCtrl', ['$scope', '$http', function($scope, $http){
         my_url += '/' + "cs61a-ayc";
         my_url += '/' + "Tak960624";
 
-        $http.get(my_url).success(function(data){
+        $scope.data = ["Loading..."];
+        progressBar('data');
+
+        $http.get(my_url).success(function(data) {
             console.log(data);
             callback = (callback) ? callback : displayData;
+            $scope.connected = true;
+
             callback();
         });
     };
 
-    var displayData = function(){
-        if ($scope.executions > 3) {
+    var progressBar = function(str) {
+        var value = 0;
+        var fadeIn = false;
+        $interval(function() {
+            value += 10;
+            if (!fadeIn) {
+                $('#' + str + 'Progressbar').fadeIn(0);
+                fadeIn = true;
+            }
+            if (value == 100) {
+                $('#' + str + 'Progressbar').fadeOut(500);
+                fadeIn = false;
+            }
+            $('#' + str + 'Progressbar').progressbar({
+                value: value
+            });
+        }, 100, 10);
+    };
+
+    var displayData = function() {
+        if ($scope.executions >= 2) {
             $scope.setConnection();
             $scope.executions = 0;
         } else {
+            progressBar('data');
+            $scope.data = ["Loading"];
             $scope.executions++;
             $http.get('/command/' + 'ls' + _dir).success(function (data) {
-                $scope.data = data.split("\n");
+                data = data.split("\n");
+                data.pop();
+                $scope.data = data;
             });
         }
     };
 
-    var readFile = function(){
-        if ($scope.executions >= 3) {
+    var readFile = function() {
+        if ($scope.executions >= 2) {
             $scope.setConnection(readFile);
             $scope.executions = 0;
         } else {
+            $scope.file = ["Loading..."];
+            progressBar('file');
             $scope.executions++;
             $http.get('/command/' + 'cat' + _dir.substr(0, _dir.length - 3)).success(function (data) {
-                $scope.file = data;
+                console.log(data);
+                $scope.file = data.split("\n");
             });
         }
+    };
+
+    $scope.goBack = function() {
+        if (history.length == 1)
+            $scope.canGoBack = false;
+        _dir = history.pop();
+        console.log(history);
+        $scope.file = "";
+        displayData();
     };
 
     $scope.open = function(name) {
@@ -61,9 +104,25 @@ visualSSH.controller('infoCtrl', ['$scope', '$http', function($scope, $http){
             _dir += " " + name + "%2F";
         }
         if (name.indexOf(".") == -1) {
+            history.push(_dir);
+            console.log(history);
+            $scope.canGoBack = true;
             displayData();
         } else {
             readFile();
         }
-    }
+    };
+
+    $scope.glookup = function(options) {
+        progressBar('file');
+        $scope.file = ["Loading..."];
+        $http.get('/command/' + 'glookup' + options).success(function (data) {
+            $scope.file = data.split("\n");
+        });
+    };
+
+    $interval(function() {
+        $('.button').button();
+    }, 10);
 }]);
+
